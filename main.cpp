c56fd9e40cfe7156f8e9ee775b8ca26f5b2c4aab
dawidlouisbekker <EDUV4947097@vossie.net>
2024-10-03 17:54:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,117 +1,62 @@
 #include <iostream>
 #include <string>
-#include <utility>
-//#include <initializer_list>
-
-using namespace std;
-
-std::string line = "";
-bool on = false;
-
-template <typename... Words>    //std:c++ 17 or higher
-bool IsInputStringThis(std::string line, Words... words) {
-	return ((words == line) || ...);
-};
 
 
-
-template <typename... Words> //std:c++ 17 or higher for the "...", it is an args list
-bool IsInputStreamThis(iostream& in_cin, Words... words) { //Program will freeze if you use this because '&' is in iostream& so it locks memory I think (refrence variable) and does not make copy.
-	string line;
-	getline(in_cin, line);
-	return ((words == line) || ...);
-};
+bool isOn = false;
 
 template <typename... Words>
-bool IsInputThis(Words... words) {
-	string line;
-	getline(std::cin, line);
-	return ((words == line) || ...);
-};
-
-static void PromptUser() {
-	cout << "Turn on light bulb (yes, Yes , y, no, No, n): ";
-}
-///////////////////////////////////////////////////////////Example
-
-
-bool prompt_user_with_and_use_IsInputStringThis() {
-	PromptUser();
-	getline(std::cin, line);
-	if (IsInputStringThis(line, "Yes", "yes", "y")) {
-		on = true;
-		cout << "Turned Light Bulb on" << endl;
-		//could also do this // return false;
-	}
-	if (IsInputStringThis(line, "No, no, n")) {
-		on = false;
-		cout << "Turned Light Bulb off";
-		//could also do this // return false;
-	}
-	if (on != true && on != false) {
-		cout << "You did not input a valid option please try again" << endl;
-		false;
-	}
-	else {
-
-		return true; }
-};
-
-int main() {
-	//example
-	bool isfinished = false;
-
-		 //accesses you cmd and puts in cls basically
-	prompt_user_with_and_use_IsInputStringThis();
-	system("cls");
-	cout << "type 'exit' to quite" << endl;
-	string choice;
-	while (isfinished == false) {
-		cout << "Would you like to turn on the light? (type 'yes', 'Yes', 'YES', 'yEs' or 'no','No','N','n','nO')" << endl;
-		getline(cin, choice);
-		if (IsInputStringThis(choice, "yes", "Yes", "YES", "yEs")) //Bag of words
-		{
-			if (on = true) {
-				cout << "Light is already on" << endl;
-			}
-			if(on = false) {
-				cout << "Turned on light" << endl;
-			}
-			
-		}; 
-
-		if (IsInputStringThis(choice, "no", "No", "N", "n", "nO")) //Bag of words
-		{
-			if (on = true) {
-				cout << "Light is already on" << endl;
-			}
-			if (on = false) {
-				cout << "Turned on light" << endl;
-			}
-		};
-
-		if (IsInputStringThis(choice, "exit")) //Bag of words
-		{
-			system("cls");
-			cout << "Exiting";
-			isfinished = true;
-		};
-		
-		//almost like bag of words
-	};
-
-	//Mine
-
-
-	return 1;
+bool IsInputStringThis(const std::string line, Words... words) {
+    return ((line == words) || ...);
 }
 
-/*bool isValid(iostream cin) {
-	std::string line;
-	getline(cin, line);
-	int asciiVal;
-	for (char ch : line) {
-		asciiVal = static_cast<int>(ch);
-	}
+
+int main()
+{   
+    
+    bool isfinished = false;
+    std::string inputLine;
+
+
+    std::cout << "Enter 'exit' to exit" << std::endl;
+    while (isfinished == false) {
+        std::cout << "type 'off' to turn switch off, 'on' to turn switch on: ";
+        std::getline(std::cin, inputLine);
+        bool wasRead = false;
+        if (IsInputStringThis<std::string>(inputLine, "On", "on", "oN", "ON") == true) {                  //Bag Of words          
+            if (isOn == true) {
+                std::cout << "turned on" << std::endl;
+            }
+            if (isOn == false) {
+                std::cout << "already turned on" << std::endl;
+            }
+            isOn = true;
+            wasRead = true;
+        }
+
+        if (IsInputStringThis<std::string>(inputLine, "off", "OFF", "oFF", "Off", "oFf") == true) {
+            if (isOn == true) {
+                std::cout << "turned off" << std::endl;
+            }
+            if (isOn == false) {
+                std::cout << "Already turned off" << std::endl;
+            }
+           
+            wasRead = true;
+        }
+
+        if (wasRead == false) {
+            std::cout << "You did not give a valid response. Please enter off or on." << std::endl;
+        }
+        if (IsInputStringThis(inputLine,"exit")) {
+            system("cls");
+            isfinished = true;
+        }
+
+
+
+    }
+      //using std::cout in not using namespace std;
+
+    
+
 }
-*/
